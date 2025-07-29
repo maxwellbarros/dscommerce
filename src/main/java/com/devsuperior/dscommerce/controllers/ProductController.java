08031ea1e63cd409b42fcb9943cdf26f798b6349
maxwellbarros <maxwellbarros@yahoo.com.br>
2025-07-29 11:38:27 -0300

    GET product by id

---
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dscommerce.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.services.ProductService;
+
+@RestController
+@RequestMapping(value="/products")
+public class ProductController {
+
+//	@Autowired
+//	private ProductRepository repository;
+	
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping(value="/{id}")
+	public ProductDTO findById(@PathVariable Long id) {	
+		return service.findById(id);
+		
+	}
+}
