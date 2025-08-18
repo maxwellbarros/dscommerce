2cf40e676765801cc2bc006537f2eaeae1f5a4c8
maxwellbarros <maxwellbarros@yahoo.com.br>
2025-08-18 17:49:09 -0300

    Bean Validation

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -18,6 +18,8 @@ import java.net.URI;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.services.ProductService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping(value="/products")
 public class ProductController {
@@ -43,7 +45,7 @@ public class ProductController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {	
+	public ResponseEntity<ProductDTO> insert(@Valid  @RequestBody ProductDTO dto) {	
 		dto =  service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
@@ -51,7 +53,7 @@ public class ProductController {
 	}
 	
 	@PutMapping(value="/{id}")
-	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {	
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) {	
 		dto = service.update(id,dto);
 		return ResponseEntity.ok(dto);
 		
